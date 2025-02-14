fbbdb8f9b36dda5255423d7d5d6cd0262aceabd9
Tushar Saini <mail.honey6401@gmail.com>
2025-02-14 11:46:03 +0530

    Updates

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,16 @@
-import express from "express";
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config(); 
 
 const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
+const PORT = process.env.PORT; 
+
+app.get('/', (req, res) => {
+  res.send('Server is running...');
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
