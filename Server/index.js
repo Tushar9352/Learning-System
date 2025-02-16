07cb9936a1e0a724079ead3ca2850b9c8c42a3bc
Tushar Saini <mail.honey6401@gmail.com>
2025-02-16 11:09:17 +0530

    Updates

--- Server/index.js
+++ Server/index.js
@@ -1,16 +1,22 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express"
+import dotenv from "dotenv"
 
-dotenv.config(); 
+dotenv.config()
 
-const app = express();
+const app = express()
 
-const PORT = process.env.PORT; 
+const PORT = process.env.PORT
 
-app.get('/', (req, res) => {
-  res.send('Server is running...');
-});
+if (!PORT) {
+  console.error("PORT is not defined in the .env file")
+  process.exit(1)
+}
+
+app.get("/", (req, res) => {
+  res.send("Server is running...")
+})
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
+
