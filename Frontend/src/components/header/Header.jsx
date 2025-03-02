c22b76b99c267052f2b94b2e962c15799ee78e1f
Tushar Saini <mail.honey6401@gmail.com>
2025-03-02 18:13:53 +0530

    Updated Files

--- Frontend/src/components/header/Header.jsx
+++ Frontend/src/components/header/Header.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import "./header.css";
 import { Link } from "react-router-dom";
 
-// eslint-disable-next-line react/prop-types
 const Header = ({ isAuth }) => {
   return (
     <header>
--- Frontend/src/components/testimonials/Testimonials.jsx
+++ Frontend/src/components/testimonials/Testimonials.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 import "./testimonials.css";
 
@@ -10,7 +11,7 @@ const Testimonials = () => {
       message:
         "This platform helped me learn so effectively. The courses are amazing and the instructors are top-notch.",
       image:
-        "https://th.bing.com/th?q=Current+Bachelor&w=120&h=120&c=1&rs=1&qlt=90&cb=1&dpr=1.3&pid=InlineBlock&mkt=en-IN&cc=IN&setlang=en&adlt=moderate&t=1&mw=247",
+        "https://i.pinimg.com/474x/51/21/17/512117705377578681235dc6e94fa3b5.jpg",
     },
     {
       id: 2,
@@ -19,16 +20,16 @@ const Testimonials = () => {
       message:
         "I've learned more here than in any other place. The interactive lessons and quizzes make learning enjoyable.",
       image:
-        "https://th.bing.com/th/id/OIP.GKAiW3oc2TWXVEeZAzrWOAHaJF?w=135&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        "https://i.pinimg.com/474x/54/04/b1/5404b179cf8057a7f6c258493fd515f0.jpg",
     },
     {
       id: 3,
-      name: "John Doe",
+      name: "Bruce Banner",
       position: "Student",
       message:
         "This platform helped me learn so effectively. The courses are amazing and the instructors are top-notch.",
       image:
-        "https://th.bing.com/th?q=Current+Bachelor&w=120&h=120&c=1&rs=1&qlt=90&cb=1&dpr=1.3&pid=InlineBlock&mkt=en-IN&cc=IN&setlang=en&adlt=moderate&t=1&mw=247",
+        "https://i.pinimg.com/474x/f1/c4/e7/f1c4e7b5f5b61c8d5ca4fc25e5346fd8.jpg",
     },
     {
       id: 4,
@@ -37,7 +38,7 @@ const Testimonials = () => {
       message:
         "I've learned more here than in any other place. The interactive lessons and quizzes make learning enjoyable.",
       image:
-        "https://th.bing.com/th/id/OIP.GKAiW3oc2TWXVEeZAzrWOAHaJF?w=135&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        "https://i.pinimg.com/474x/cd/dc/cb/cddccb92dda5bee511ab612d724b7064.jpg",
     },
   ];
   return (
