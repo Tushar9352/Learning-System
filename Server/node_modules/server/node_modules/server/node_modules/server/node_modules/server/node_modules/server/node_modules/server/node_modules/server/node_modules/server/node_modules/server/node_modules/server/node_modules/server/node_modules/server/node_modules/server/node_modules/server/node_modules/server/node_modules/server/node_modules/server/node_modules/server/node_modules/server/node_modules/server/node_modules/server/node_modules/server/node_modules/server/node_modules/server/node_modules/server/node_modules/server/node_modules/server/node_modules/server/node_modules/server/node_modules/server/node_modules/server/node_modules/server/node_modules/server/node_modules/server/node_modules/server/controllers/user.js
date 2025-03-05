62920813c9d1db101cd142027221316a68baef08
Tushar Saini <mail.honey6401@gmail.com>
2025-03-05 15:04:36 +0530

    Updates

--- Server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/controllers/user.js
+++ Server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/node_modules/server/controllers/user.js
@@ -76,7 +76,8 @@ export const verifyUser = TryCatch(async (req, res) => {
 
 export const loginUser = TryCatch(async (req, res) => {
   const { email, password } = req.body;
-    const user = await User.findOne({ email });
+
+  const user = await User.findOne({ email });
 
   if (!user)
     return res.status(400).json({
